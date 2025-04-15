d0fa5726782cd245fd9297a057fb83aca3c20481
daVinciManuel <mgmt.manuel02@gmail.com>
2025-04-15 21:15:38 +0200

    ADD login form

---
+++ views/vlogin.php
@@ -0,0 +1,15 @@
+<?php
+echo '<!DOCTYPE html>';
+echo '<html lang="es">';
+echo '  <head>';
+echo '    <meta charset="UTF-8">';
+echo '    <meta name="viewport" content="width=device-width,initial-scale=1.0">';
+echo '    <meta name="author" content="Manuel Martinez">';
+echo '    <meta name="" content="">';
+echo '  </head>';
+echo '  <body>';
+echo '  <form name="login" method="POST">';
+// continuar aqui
+echo '  </form>';
+echo '  </body>';
+echo '</html>';
